72a1c0c16c1e3447b8fa79947837275df83e2535
MercuryLee <sooseong1230@gmail.com>
2023-09-20 15:30:24 +0900

    .

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title: Text(
+          "홈",
+          style: TextStyle(
+
+          )
+        )
+      ),
+      body: const Column(
+        children: [
+
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items:const [
+          BottomNavigationBarItem(
+            icon : Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            icon : Icon(Icons.add_comment),
+          ),
+          BottomNavigationBarItem(
+            icon : Icon(Icons.more_horiz),
+          ),
+        ]
+      ),
+    );
+  }
+}
