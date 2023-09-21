05be60dd753102c574d4607f3c9161a142841a44
jinhyeok77 <ljh01152097@gmail.com>
2023-09-21 17:16:11 +0900

    page view

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -6,12 +6,75 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("홈", style: TextStyle(fontSize: 26))),
-      body: PageView(
-        scrollDirection: Axis.horizontal,
-        children: [
-          SizedBox(),
-        ],
+      appBar: AppBar(title: const Text("홈", style: TextStyle(fontSize: 26))),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 196,
+                child: PageView(
+                  scrollDirection: Axis.horizontal,
+                  physics: BouncingScrollPhysics(),
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(24.0),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(16),
+                        ),
+                        color: Colors.green[200],
+                      ),
+                      child: const Text(
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 20,
+                        ),
+                        'first',
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(24.0),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(16),
+                        ),
+                        color: Colors.green[200],
+                      ),
+                      child: const Text(
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 20,
+                        ),
+                        'first',
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(24.0),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(16),
+                        ),
+                        color: Colors.green[200],
+                      ),
+                      child: const Text(
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 20,
+                        ),
+                        'first',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: "홈"),
